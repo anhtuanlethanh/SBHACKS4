3acb1ac848d9192b186cfdee10d9f9a7651b90c9
IlMatteo <MatthewPontarolo@gmail.com>
2018-01-19 21:04:52 -0800

    First working new window

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-const {app, BrowserWindow} = require('electron') // http://electron.atom.io/docs/api
-const path = require('path')         // https://nodejs.org/api/path.html
-const url = require('url')           // https://nodejs.org/api/url.html
+const {app, BrowserWindow, ipcMain} = require('electron'); // http://electron.atom.io/docs/api
+const path = require('path');         // https://nodejs.org/api/path.html
+const url = require('url');           // https://nodejs.org/api/url.html
 
-let window = null
+let window = null;
+
+ipcMain.on('screen:add', function(e, eventName, year) {
+  window.loadURL('file://' + __dirname + '/addScreen.html');
+});
 
 // Wait until the app is ready
 app.once('ready', () => {
   // Create a new window
   window = new BrowserWindow({
-    // Set the initial width to 400px
     width: 700,
-    // Set the initial height to 500px
     height: 500,
     // Don't show the window until it ready, this prevents any white flickering
     show: false,
     // Don't allow the window to be resized.
     resizable: false,
-  })
+  });
 
   // Load a URL in the window to the local index.html path
   window.loadURL(url.format({
     pathname: path.join(__dirname, 'popup.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Show window when page is ready
   window.once('ready-to-show', () => {
     window.show()
-  })
-})
+  });
+});
--- window.js
+++ window.js
@@ -1,1 +1,18 @@
+/*const ipcMain = require('electron').ipcMain;
+const ipc = require('electron').ipcRenderer;
+ipc.on('load-page', (event, arg) => {
+    mainWindow.loadURL(arg);
+});
 
+function goToAddScreen(e) {
+  console.log("test");
+  mainWindow.loadUrl('file://' + __dirname + '/addScreen.html');
+  //ipc.send('load-page', 'file://' + __dirname + '/addScreen.html');
+}*/
+
+const {ipcRenderer} = require('electron');
+
+function goToAddScreen(e) {
+  console.log("test");
+  ipcRenderer.send('screen:add');
+}
