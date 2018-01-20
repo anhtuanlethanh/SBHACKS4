7be216275d4da94a4d330c77e1488612feccd8dc
IlMatteo <MatthewPontarolo@gmail.com>
2018-01-19 21:45:51 -0800

    Removed some clutter

--- window.js
+++ window.js
@@ -1,15 +1,3 @@
-/*const ipcMain = require('electron').ipcMain;
-const ipc = require('electron').ipcRenderer;
-ipc.on('load-page', (event, arg) => {
-    mainWindow.loadURL(arg);
-});
-
-function goToAddScreen(e) {
-  console.log("test");
-  mainWindow.loadUrl('file://' + __dirname + '/addScreen.html');
-  //ipc.send('load-page', 'file://' + __dirname + '/addScreen.html');
-}*/
-
 const {ipcRenderer} = require('electron');
 
 function goToAddScreen(e) {
